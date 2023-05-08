09b1b871c6df122b0feb8e087dbd00a4113dd0e5
期贤 <qixian.cs@antgroup.com>
2023-05-08 11:30:45 +0800

    优化背景颜色等试用体验

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import './globals.css';
 import { Inter } from 'next/font/google';
 
@@ -15,6 +16,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:title" content="miu 姐帮你看" key="title" />
+        <meta
+          property="og:image"
+          content="https://mdn.alipayobjects.com/huamei_gcee1x/afts/img/A*88xbQaYvP4YAAAAAAAAAAAAADml6AQ/original"
+          key="image"
+        />
+        <meta property="og:description" content="看完下班开罐罐" />
+        <meta property="og:type" content="article" />
+      </Head>
       <body
         className={inter.className}
         style={{
